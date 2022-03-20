ec06a449d9a0d9309f633fc5bcab66d72dad7bc3
Enrico Notaro <enriconotaro@gmail.com>
2022-03-20 18:10:56 +0100

    first commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,4 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import axios from "axios";
 // sass files
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -7,8 +6,67 @@ import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-export {
-    checkForName,
-    handleSubmit
-   }
+let nations = [];
+let codes = [];
+let nationSelected='';
+let codeNationSelested='';
+
+//  ASYNC GET
+const getCodeOrNation = async (url = '') => {
+    const request = await fetch(url);
+    try {
+        const allData = await request.json();
+        return allData;
+    } catch (error) {
+        console.log("error", error);
+    }
+
+};
+async function NationsAndCodes() {
+    getCodeOrNation('/Nation').then(function (allData) {
+        nations = allData;
+        console.log(nations)
+    });
+    getCodeOrNation('/Code').then(function (allData) {
+        codes = allData;
+        console.log(codes)
+    });
+
+};
+NationsAndCodes();
+
+function construct() {
+    const fragment = document.createDocumentFragment();
+    for (var i = 0; i < 239; i++) {
+        const newElement = document.createElement('option');
+        newElement.setAttribute('value', codes[i]);
+        newElement.textContent = nations[i];
+        fragment.appendChild(newElement);
+    };
+    document.querySelector("#Nazioni").appendChild(fragment);
+};
+setTimeout(() => { construct(); value = e.options[e.selectedIndex].value; }, 800);
+
+let e = document.getElementById("Nazioni");
+let cod = document.getElementById("cod");
+e.addEventListener('change', () => {
+    let value = e.options[e.selectedIndex].value.substr(0,2)  ;
+    cod.textContent =value;
+    console.log(value);
+    codeNationSelested=value;
+    console.log( e.options[e.selectedIndex].textContent);
+    nationSelected=e.options[e.selectedIndex].textContent;
+
+});
+
+let submit=document.getElementById("submt");
+submit.addEventListener("click",()=>{
+    let period=document.querySelector('#range').value;
+    console.log(period)
+    let city=document.getElementById("Inputcity").value;
+    console.log(city);
+    //send code and nation to server
+    axios.post('/dataForGeoname', {city: city,nation:nationSelected, code:codeNationSelested,period:period}).then(function(res) {
+        console.log(res.data)
+    })
+});
--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,32 @@
+const axios = require('axios');
 var path = require('path');
 const express = require('express');
+const readXlsxFile = require('read-excel-file/node');
 const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
 dotenv.config();
+//nvm use --lts
 
-const port=8081;
+let GEONAMES_BASEURL="http://api.geonames.org/searchJSON?q=";
+let GEONAMES_CITY="";
+let GEONAMES_PARAMS="&maxRows=10&fuzzy=0.8&";
+let GEONAMES_USERNAME=process.env.GEONAMES_USERNAME;
+
+let lngGEONAMES=null;
+let latGEONAME=null;
+let City=null;
+let DateArrive=null;
+let DateDeparture=null;
+let PopulationGEONAMES=null;
+
+let API_KEY_WEATHERBIT=process.env.API_KEY_WEATHERBIT;
+
+let API_KEY_PIXABAY=process.env.API_KEY_PIXABAY;
+
+const port=8080;
 const app = express();
-// Cors allows the browser and server to communicate without any security interruptions
-const cors = require('cors');
+const cors = require('cors');// Cors allows the browser and server to communicate without any security interruptions
 
 app.use(express.static('dist'));
 app.use(cors());
@@ -16,10 +34,9 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 console.log(__dirname);
 // API
-const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?';
-const API_key = process.env.API_KEY;
-console.log(`Your API Key is ${process.env.API_KEY}`);
-let URLforNLP = [] ;
+
+///console.log(`Your API Key is ${process.env.API_KEY}`);
+
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -35,13 +52,103 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse);
 });
 // POST Route
-app.post('/NPL', async function(req, res) {
+app.post('/dataForGeoname', async function(req, res) {
     console.log('***************POST 0K******************');
-    let url_to_send=req.body.url;
-    //console.log(url_to_send);
-    let NPLurl=`${baseURL}key=${API_key}&url=${url_to_send}&lang=en`;
-    const response = await fetch(NPLurl);
-    const Data = await response.json();
-    //console.log(Data);
-    res.send(Data);
-});
+
+    //Data for APIs
+    let city=req.body.city;
+    let nation=req.body.nation;
+    let code=req.body.code;
+    let period=req.body.period;
+    let DateArrive=new Date(period.substr(0, 10).trim());
+    let DateDeparture= new Date(period.substr(13, 10).trim());
+    let totalDaysInCity=(DateDeparture-DateArrive)/(24*3600000);
+    console.log(city+' '+nation+' '+code+' '+period);
+    console.log(DateArrive,'--',DateDeparture)
+    
+    //responses of APIs inizialized
+    let responseGeoname='';
+    let responseWeatherbit='';
+    //let responseWeatherbitForManyDays='';
+
+    //API Geoname
+    GEONAMES_CITY=city;
+    let getGeoname= GEONAMES_BASEURL+GEONAMES_CITY+GEONAMES_PARAMS+'username='+GEONAMES_USERNAME
+    await axios.get(getGeoname).then(resp => {
+       
+        responseGeoname=resp.data;
+        
+      })
+      .catch(err => {
+        res.end(JSON.stringify({err : "There was some error"}));
+      })
+      //This code select the correct city with code of Nation
+      let correctCity=''
+      for(let element of responseGeoname['geonames']){//provare con il metodo find
+          if (element['countryCode']===code){
+            correctCity=element;  
+            break   
+          }
+      }
+      console.log(correctCity);
+      lngGEONAMES=correctCity['lng'];
+      latGEONAME=correctCity['lat'];
+      PopulationGEONAMES=correctCity['population'];
+      city=correctCity['name']
+      res.send('OK****************************');
+
+    // API Weatherbit
+     //current
+    const url = `https://api.weatherbit.io/v2.0/current?lat=${latGEONAME}&lon=${lngGEONAMES}&key=${API_KEY_WEATHERBIT}`;
+    await axios.get(url).then(resp => {
+        responseWeatherbit=resp.data;
+        // console.log(responseWeatherbit);
+        // console.log(responseWeatherbit['data'][0]['temp']);
+        // console.log(responseWeatherbit['data'][0]['datetime']);
+        //console.log(responseWeatherbit['data'][0]['name']);
+        
+    })
+    .catch(err => {
+      res.end(JSON.stringify({err : "There was some error"}));
+    })
+    
+    //API for Pix
+
+    const urlPIXABAY=`https://pixabay.com/api/?key=${API_KEY_PIXABAY}&q=${city}&image_type=photo`;;
+   
+    await axios.get(urlPIXABAY).then(resp => {
+      responsePIXABAY=resp.data;
+      // console.log(responsePIXABAY);
+      // console.log(responsePIXABAY['hits'][0]['webformatURL'])
+    })
+    .catch(err => {
+    res.end(JSON.stringify({err : "There was some error"}));
+    })
+    console.log(totalDaysInCity);
+    console.log(DateArrive);
+    console.log(DateDeparture);
+  });
+
+//the nations and codes
+
+let nations=[];
+let nationCodes=[];
+
+readXlsxFile('CountryCode.xlsx').then((rows) => {
+    for(var i=1;i<240;i++){
+        nations.push(rows[i][0]);
+        nationCodes.push(rows[i][2]);
+    };
+    //console.log(nations)
+ }
+);
+ //GET request
+ app.get('/Nation',reqNations);
+ function reqNations(req,res){
+     res.send(nations);
+ };
+
+ app.get('/Code',reqCodes);
+ function reqCodes(req,resp){
+     resp.send(nationCodes);
+ };
